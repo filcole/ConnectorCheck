8c871e3e1c1fbe0297e1bea64eeaf51f7475d243
Phil Cole <filcole@gmail.com>
2021-08-10 22:03:49 +0100

    Output table

--- depcheck.ps1
+++ depcheck.ps1
@@ -6,12 +6,30 @@ param (
 
 )
 
-Function Get-DeepProperty([object] $InputObject, [string] $Property) 
-{
+$hash = @{}
+$usedAction = @{}
+
+Function Get-DeepProperty([object] $InputObject, [string] $Property) {
     $path = $Property -split '\.'
     $obj = $InputObject
-    $path | %{ $obj = $obj.$_ }
+    $path | % { $obj = $obj.$_ }
     $obj
+}
+
+Function StoreUsedAction {
+    param ([string]$connector, [string]$action)
+
+    $key = "${connector}|${action}"
+    
+    # FIXME: This is pretty lame, but so is my powershell skills
+    if ( $usedAction.ContainsKey(${key}) ) {
+        $count = $usedAction[${key}]
+        $newCount = $count + 1
+        $usedAction[$key] = $newCount
+    }
+    else {
+        $usedAction.add(${key}, 1)
+    }
 }
 
 Function log-DeprecatedActions {
@@ -24,8 +42,6 @@ Function log-DeprecatedActions {
         $actionObjects = Get-Member -InputObject $actions -MemberType NoteProperty
 
         foreach ($actionObject in $actionObjects) {
-            #$message = "Action: " + $actionObject.Name 
-            #Write-Host $message.PadLeft($depth, " ")
 
             $actionBody = $actions | Select-Object -ExpandProperty $actionObject.Name
 
@@ -41,6 +57,10 @@ Function log-DeprecatedActions {
                 $propertyPath = "properties.connectionReferences." + $connectionRef + ".api.name"
                 $connector = Get-DeepProperty -InputObject $flow -Property $propertyPath
 
+                if ($connector.StartsWith("shared_")) {
+                    $connector = $connector.Replace("shared_", "")
+                }
+
                 $connRefLogicalNamePath = "properties.connectionReferences." + $connectionRef + ".connection.connectionReferenceLogicalName"
                 $connRefLogicalName = Get-DeepProperty -InputObject $flow -Property $connRefLogicalNamePath
 
@@ -50,21 +70,23 @@ Function log-DeprecatedActions {
                 if ($hash.ContainsKey($key)) { 
                     Write-Host "Deprecated action in flow ${filename}: ${operationId} on connector ${connector} (connectionRef: ${connRefLogicalName}) in step ${actionName}: ${description}"
                 }
-            }
 
-            # $message = " " * $depth + "<${type}>: ${description}"
-            # Write-Host $message
-
-            # Does this action have an "actions" node below it?
-            $actionNodes = Get-Member -InputObject $actionBody -MemberType NoteProperty
-            $childActions = $actionNodes | Where-Object -Property Name -eq -Value "actions" 
-            if ($childActions -ne $null) {
-                # We have child actions - recurse into actions list and drill through these actions
-                [int]$newDepth = $depth + 2
-                $childActionObject = $actionBody | Select-Object -ExpandProperty $childActions.Name
-                # Recurse
-                log-DeprecatedActions -depth $newDepth -actions $childActionObject
+                StoreUsedAction $connector $operationId
             }
+        }
+
+        # $message = " " * $depth + "<${type}>: ${description}"
+        # Write-Host $message
+
+        # Does this action have an "actions" node below it?
+        $actionNodes = Get-Member -InputObject $actionBody -MemberType NoteProperty
+        $childActions = $actionNodes | Where-Object -Property Name -eq -Value "actions" 
+        if ($childActions -ne $null) {
+            # We have child actions - recurse into actions list and drill through these actions
+            [int]$newDepth = $depth + 2
+            $childActionObject = $actionBody | Select-Object -ExpandProperty $childActions.Name
+            # Recurse
+            log-DeprecatedActions -depth $newDepth -actions $childActionObject
         }
     }
 }
@@ -73,7 +95,7 @@ $deprecatedfilename = Join-Path ${PSScriptRoot} "Deprecated.json"
 
 $deprecatedActions = Get-Content $deprecatedfilename | ConvertFrom-Json
 
-$hash = @{}
+
 
 $deprecatedActions | Foreach-Object {
     $connector = $_;
@@ -103,11 +125,22 @@ Get-ChildItem $workflowfolder -Filter *.json |
 Foreach-Object {
     $filename = $_.FullName
 
-    Write-Progress "Checking flow "$filename
+    #Write-Progress "Checking flow "$filename
 
     $flow = Get-Content $_.FullName | ConvertFrom-Json
 
     $actions = $flow.properties.definition.actions
 
     log-DeprecatedActions -depth 2 -actions $actions
+}
+
+Write-Output "`nSummary:`n"
+
+$usedAction.GetEnumerator() | ForEach-Object {
+    $isDeprecated = "";
+    if ($hash.ContainsKey($_.key)) {
+        $isDeprecated = "*** DEPRECATED *** "
+    }
+    $message = "{1,3} usages of {2}{0}" -f $_.key, $_.value, $isDeprecated
+    Write-Output $message
 }
